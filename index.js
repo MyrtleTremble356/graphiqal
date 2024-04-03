c12de4f0b237f44ae708851eb5f6099930bdeedc
Yin <doyin@deloitte.com.cn>
2024-04-03 14:24:07 +0800

    initial version

---
+++ index.js
@@ -0,0 +1,7 @@
+const formatDate = require('./formatDate')
+const truncateString = require('./truncateString')
+const stringToNumberArray = require('./stringToNumberArray')
+const isEven = require('./isEven')
+const getMaxValue = require('./getMaxValue')
+
+module.exports = (msg) => console.log(msg)
